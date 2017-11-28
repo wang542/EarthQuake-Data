ab27c3a9c1c483d2436eb937a2212d780c0b311d
zhuzhic <zhuzhic@IULB-I109-23.ads.iu.edu>
2017-11-28 09:21:54 -0500

    "update"

--- src/application/EarthquakeDataController.java
+++ src/application/EarthquakeDataController.java
@@ -1,9 +1,17 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class EarthquakeDataController {
 	
@@ -27,5 +35,43 @@ public class EarthquakeDataController {
 	private Button ReportOfSelected;
 	@FXML
 	private Button ReportOfAll;
+	@FXML
+	private void ReportOfSelectedClick(ActionEvent event)
+	{
+		System.out.println("Report Of Selected data");
+		
+		// get an FXML loader and read in the fxml code
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Repoter.fxml"));
+		AnchorPane dialogRoot;
+		
+		try {
+			dialogRoot = (AnchorPane) loader.load();
+			Scene dialogScene = new Scene(dialogRoot);
+			Stage dialogStage = new Stage();
+			dialogStage.setScene(dialogScene);
+			dialogStage.show();
+			System.out.println("After dialogStage.show()");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+			
+	}
+	
+}
+public void writeNewUser() {      
+    PrintWriter fw = null;
 
+    try {
+        fw = new PrintWriter("users.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(tfUsername.getText());
+        bw.newLine();
+        bw.write(tfPassword.getText());
+        fw.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+
+    }
 }
--- src/application/RepoterController.java
+++ src/application/RepoterController.java
@@ -1,7 +1,18 @@
 package application;
-package ;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class RepoterController {
 
