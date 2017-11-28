cc91a816a35ecc3aec833ed50faeba5ee37ba234
ajdonis <ajdonis@indiana.edu>
2017-11-28 08:48:00 -0500

    Updated on 11/28

--- src/application/EarthquakeDataController.java
+++ src/application/EarthquakeDataController.java
@@ -1,5 +1,41 @@
 package application;
 
-public class EarthquakeDataController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 
+public class EarthquakeDataController {
+	
+	@FXML
+	private AnchorPane mainPane;
+	@FXML
+	private Button SelEventButton;
+	@FXML
+	private Button AllEventButton;
+	@FXML
+	private TextField Date;
+	@FXML
+	private TextField Location;
+	@FXML
+	private TextField Depth;
+	@FXML
+	private TextField Mag1;
+	@FXML
+	private TextField Mag2;
+	@FXML
+	private TextField MagType;
+	@FXML
+	private TextField Place;
+	@FXML
+	private TextField Status;
+	@FXML
+	private TextField SelEventReport;
+	@FXML
+	private TextField AllEventReport;
+	@FXML
+	private TextArea ReportArea;
+		
+	
 }
