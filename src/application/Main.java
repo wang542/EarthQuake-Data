5441069bc8163b74d6c0c1d65ceb908246d8cfda
ajdonis <ajdonis@indiana.edu>
2017-11-28 09:13:08 -0500

    Updated on 11/28

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,22 +8,30 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// set a title for the Window
+			primaryStage.setTitle("Earthquake Data");
+			
+			// get an FXML loader and read in the fxml code
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("EarthquakeData.fxml"));
+			AnchorPane mainLayout = (AnchorPane)loader.load();
+			
+			// Create the scene with the layout in the fxml code, set the scene and show it
+			Scene scene = new Scene(mainLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
